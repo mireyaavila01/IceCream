6a12d3d15cb1864f7865e01f0e477f8fefbd3798
Mireya Avila <mireyaavila147@yahoo.com>
2024-05-06 16:11:34 -0400

    working on formulas

--- scripts/site.js
+++ scripts/site.js
@@ -10,6 +10,8 @@ const inputWhippedCream = document.getElementById("inputWhippedCream");
 const inputHotFudge = document.getElementById("inputHotFudge");
 const inputCherry = document.getElementById("inputCherry");
 
+const toppingOptions = document.getElementById("toppingOptions");
+
 //button
 
 const submitOrderBtn = document.getElementById("submitOrderBtn");
@@ -21,20 +23,80 @@ const outputTax = document.getElementById("outputTax");
 const outputTotalDue = document.getElementById("outputTotal Due");
 
 window.onload = function(){
-    let cupCheckbox = document.getElementById("inputCup");
-    cupCheckbox.onclick = onHideOrShowToppingOptions;
+   
+    inputCone.onclick =onHideOrShowToppingOptions;
+    inputCup.onclick = onHideOrShowToppingOptions;
+    submitOrderBtn.onclick = onSubmitOrderBtnClick;
 
-}
+};
 
 function onHideOrShowToppingOptions (){
-    let cupCheckbox = document.getElementById("inputCup").checked;
-    let toppingOptions = document.getElementById("toppingOptions");
+    let shouldBeVisible = inputCup.checked;
+    console.log(shouldBeVisible);
 
-    if (cupCheckbox.checked) {
-        toppingOptions.style.display = "block";
+    if (shouldBeVisible){
+       // toppingOptions.style.display = "block"
+        toppingOptions.style.visibility = "visible";
     }
 
-    else{
-        toppingOptions.style.display = "none";
+    else {
+       //toppingOptions.style.display = "none";
+       toppingOptions.style.visibility = "hidden";
+
     }
+
+   
 }
+
+function onSubmitOrderBtnClick(){
+    let icecreamStartCost = 2.25;
+    let numberOfScoops = inputNumberOfScoops.value 
+    
+  
+    
+    if (inputCone || inputCup){
+        icecreamStartCost = 2.25;
+    }
+    else {
+        console.log("error");
+    }
+
+    let toppingsCost = 0;
+
+    if (document.getElementById("inputSprinkles").checked){
+        toppingsCost = .50;
+    }
+    else if (document.getElementById("inputWhippedCream").checked){
+        toppingsCost = .25;
+    }
+
+    else if (document.getElementById("inputHotFudge").checked){
+        toppingsCost = 1.25;
+    }
+    else document.getElementById("inputCherry".checked {
+        toppingsCost = .25;
+    }
+
+    let= numberOfScoops = inputNumberOfScoops.value;
+    
+
+    if (numberOfScoops = 1){
+
+        scoopCost= icecreamStartCost;
+    }
+
+    else if (numberOfScoops >1){
+        scoopCost = (numberOfScoops * 1.25) - 2.25;
+    }
+
+    let tax = 1.00;
+    
+    basePrice = scoopCost + toppingsCost;
+    totalDue = basePrice + tax;
+   
+    outputBasePrice.innerHTML = basePrice;
+    outputTax.innerHTML = tax;
+    outputTotalDue.innerHTML = totalDue;
+
+}
+
